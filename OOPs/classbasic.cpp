1ba40d7a40b636567167ffa264fccf0878ad5def
Sameer Chauhan <sameerchauhan212204@gmail.com>
2023-09-18 22:47:49 +0530

    getting start with class

--- OOPs/classbasic.cpp
+++ OOPs/classbasic.cpp
@@ -4,18 +4,36 @@
 #include <iostream>
 using namespace std;
 class Testing{
-    private:
-      int a;
+    private:                     //access specifiers :private only assecible within the class
+      int a;                     //members or variables (attributes)
       float b;
-    public:
-       int c;
+    public:                      //access specifiers :publice assecible outside the class
+       int c;                    //members or variables (attributes)
        float d;
+    void set_default(void){      //function of the class (methods)
+      a=0;
+      b=0;
+    } 
+    void print_values(void){     //function of the class  (methods)
+       cout<<"a:"<<a<<endl;
+       cout<<"b:"<<b<<endl;
+       cout<<"c:"<<c<<endl;
+       cout<<"d:"<<d<<endl;
+    }
+  Testing(){                    //constructor is a function of class which initialize objects of the class
+      a=0;b=0;c=0;d=0;
+    }
 };
 
 int main(){
-    Testing test;
+    Testing test;              //creating an instance of class Testing
     // test.a=4;
-    test.c=45;
-    test.d=43;
-    cout<<test.c<<endl;
+    // test.c=45;
+    // test.d=43;
+    // cout<<test.c<<endl;
+    test.set_default();
+    test.print_values();
+
+    return 0;
+
 }
---
+++ OOPs/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+test.c=45;
+    // test.d=43;
+    // cout<<test.c<<endl;

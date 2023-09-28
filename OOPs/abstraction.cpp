2289ece9e0004919a184641a9057dbf0be559c66
Sameer Chauhan <sameerchauhan212204@gmail.com>
2023-09-28 23:13:47 +0530

    Abstraction in class

---
+++ OOPs/abstraction.cpp
@@ -0,0 +1,3 @@
+// ABSTRACTION;abstraction in the context of classes refers to the act of hiding the
+// implementation details of the user . this allows the useer to interact with the class without needing to know how it works
+// VIRTUAL FUNCTIONS:

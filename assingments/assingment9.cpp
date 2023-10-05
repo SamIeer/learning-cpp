12218dce87b4da1c63ccc85d618fee05d8873107
Sameer Chauhan <sameerchauhan212204@gmail.com>
2023-10-05 23:58:23 +0530

    working on assingment

--- assingments/assingment9.cpp
+++ assingments/assingment9.cpp
@@ -1,46 +1,62 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 // ANSWER1:PROGRAM TO STORE A STUDENTS DETAILS IN THE FILE
-class Student{
-      private:
-        string name;
-        int roll_no;
-        float grade;
-        static int total_student;
-      public:
-        Student(string n,float g){
-            name=n;roll_no=total_student;grade=g;
-            Student::total_student++;
-        }
-      friend ofstream & operator << (ofstream &,Student &);
-};
+// class Student{
+//       private:
+//         string name;
+//         int roll_no;
+//         float grade;
+//         static int total_student;
+//       public:
+//       Student(){
+//             name="";roll_no=total_student;grade=0;
+//             // Student::total_student++;
+//             }
+//         Student(string n,float g){
+//             name=n;roll_no=total_student;grade=g;
+//             Student::total_student++;
+//         }
+//       friend ofstream & operator << (ofstream &,Student &);
+// };
       
-int Student::total_student=1;
-ofstream & operator << (ofstream & file,Student & s){
-          file<<s.name<<"\t";
-          file<<s.roll_no<<"\t";
-          file<<s.grade<<"\t";
-          return file;
-      }
+// int Student::total_student=1;
+// ofstream & operator << (ofstream & file,Student & s){
+//           file<<s.name<<"\t";
+//           file<<s.roll_no<<"\t";
+//           file<<s.grade<<"\t";
+//           return file;
+//       }
 
-int main(){
-    Student S1("sameer",90.5);
-    Student s2("mohit",45.6);
-    ofstream my_file;
-    my_file.open("output.txt");
-    my_file<<S1;
-    my_file<<s2;
-    my_file.close();
-    return 0;
-}
+// int main(){
+//     cout<<"enter the number of student you want to set details of:"<<endl;
+//     int n;
+//     cin>>n;
+//     //object creation
+//     Student s[n];
 
-// ANSWER2:PROGRAM TO READ CONTENT FROM THE FILE AND PRINT IT
+//     string name;float grade;
+//     ofstream my_file;
+//     my_file.open("output.txt");
+//     for (int i = 0; i < n; i++)
+//     {
+//       cin>>name>>grade;
+//       s[i]=Student(name,grade);
+//       my_file<<s[i]<<endl;
+//     }
+//     my_file.close();
+//     return 0;
+// }
+
+// // ANSWER2:PROGRAM TO READ CONTENT FROM THE FILE AND PRINT IT
 int main(){
     ifstream inFile;
-    infile.open("output.txt");
+    string a;
+    inFile.open("output.txt");
     while(!inFile.eof()){
-        
-
+         inFile>>a;
+         cout<<a;
     }
+    return 0;
 }

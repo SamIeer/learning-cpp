1034fc43f7885bc6c4bdf7f03cecc779aad0e760
Sameer Chauhan <sameerchauhan212204@gmail.com>
2023-10-07 23:40:25 +0530

    working on assingment

--- assingments/assingment8.cpp
+++ assingments/assingment8.cpp
@@ -27,50 +27,56 @@ using namespace std;
 //    }
 
 //ANSWER2: PROGRAM TO DEMONSTRATE INTEGER EXCEPTION AND CHARACTER EXCEPTION
-// int main(){
-//     try{
-//         char ch='A';
-//         cout << "The ASCII value of A = "<<ch<< endl ;
-//         }catch (exception& e ){ // exception class object can be passed as argument to the 
-//         cout <<"Exception caught in integer Exception handler" << endl;}
-//     }
+int main() {
+ cout<<"enter the number"<<endl;
+ int n;
+ cin>>n;
+ cout<<n+1;
+ cout<<"enter the string"<<endl;
+ string s;
+ cin>>s;
+ cout<<s;
+ 
+ return 0;
+}
+
+
+
 
 
 //ASSINGMENT QUESTION : FINDING THE ROOTS OF QUARDIC EQUATION AND HANDLING THE ERROR WHEN A=0
-int find_roots(int a,int b,int c){
-    int d=(b*b)-(4*a*c);
-    float x,y;
-    if((d==0)){
-        cout<<"ROOTS ARE REAL BUT NOT EQUAL"<<endl;
-        x= -b /(2*a); 
-        x=y;
-        cout<<"roots are"<<x<<"and"<<y <<endl;
-    }
-    else if(d>0){
-       cout<<"ROOTS ARE REAL BUT UNEQUAL :"<<endl;
-       x=(-b+sqrt(d))/(2.0*a);
-       y=(-b-sqrt(d))/(2.0*a);
-        cout<<"roots are"<<x<<"and"<<y <<endl;
-    }
-    else{
-        cout<<"ROOTS ARE IMAGINARY"<<endl;
-    }
-    return 0;
-}
-
-int main(){
-    int a,b,c;
-    cout<<"ENTER THE VLAUE OF a , b  AND c respectivily :"<<endl;
-    cin >> a >> b >> c;
-    cout<<"THE QUADRIC EQUATION :"<<a<<"x^2 +"<<b<<"x +"<<c<<endl;
-    try{
-        if(a==0){
-            throw 1;
-        }
-        find_roots(a,b,c);
+// int find_roots(int a,int b,int c){
+//     int d=(b*b)-(4*a*c);
+//     float x,y;
+//     if((d==0)){
+//         cout<<"ROOTS ARE REAL BUT NOT EQUAL"<<endl;
+//         x= -b /(2*a); 
+//         x=y;
+//         cout<<"roots are"<<x<<"and"<<y <<endl;
+//     }
+//     else if(d>0){
+//        cout<<"ROOTS ARE REAL BUT UNEQUAL :"<<endl;
+//        x=(-b+sqrt(d))/(2.0*a);
+//        y=(-b-sqrt(d))/(2.0*a);
+//         cout<<"roots are"<<x<<"and"<<y <<endl;
+//     }
+//     else{
+//         cout<<"ROOTS ARE IMAGINARY"<<endl;
+//     }
+//     return 0;
+// }
+// int main(){
+//     int a,b,c;
+//     cout<<"ENTER THE VLAUE OF a , b  AND c respectivily :"<<endl;
+//     cin >> a >> b >> c;
+//     cout<<"THE QUADRIC EQUATION :"<<a<<"x^2 +"<<b<<"x +"<<c<<endl;
+//     try{
+//         if(a==0){
+//             throw 1;
+//         }
+//         find_roots(a,b,c);
     
-    } catch(int e){
-        cout<<"DIVISION WITH ZERO IS NOT POSSIBLE ,a=0"<<endl;
-    }
-    
-}
+//     } catch(int e){
+//         cout<<"DIVISION WITH ZERO IS NOT POSSIBLE ,a=0"<<endl;
+//     }
+// }

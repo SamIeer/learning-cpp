60320a72f1d3d452917ab7b6c9202889263e0617
Sameer Chauhan <sameerchauhan212204@gmail.com>
2023-11-19 22:35:41 +0530

    Assingment working

--- assingments/Assingment10.cpp
+++ assingments/Assingment10.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
-using namespace std;
-//WRITE A PROGRAMME IN C++ reeturning starting locations of a sub string using pointers
-#include <string>
-int main(){
-  stirng 
+#include <cstring>
+
+// Function to find starting locations of a substring in a string
+void findSubstring(const char* str, const char* sub) {
+    int strLength = strlen(str);
+    int subLength = strlen(sub);
+
+    if (subLength > strLength) {
+        std::cout << "Substring not found in the given string." << std::endl;
+        return;
+    }
+
+    const char* ptr = str;
+    const char* subPtr;
+
+    while (*ptr) {
+        subPtr = sub;
+        const char* tempPtr = ptr;
+
+        while (*tempPtr == *subPtr && *tempPtr && *subPtr) {
+            tempPtr++;
+            subPtr++;
+        }
+
+        if (*subPtr == '\0') {
+            // Substring found, print the starting location
+            std::cout << "Substring found at position: " << (ptr - str) + 1 << std::endl;
+        }
+
+        ptr++;
+    }
+}
+
+int main() {
+    const char* mainString = "ababcababcabc";
+    const char* substring = "abc";
+
+    std::cout << "Finding substring '" << substring << "' in string '" << mainString << "':" << std::endl;
+    findSubstring(mainString, substring);
+
+    return 0;
 }
